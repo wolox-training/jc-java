b6e17d3927e17519130a96119459f1e83c348f5b
Casanova569 <jose.casanova@wolox.com.ar>
2019-10-21 15:17:20 -0300

    change Repository for CrudRepository and add Optional to method

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,9 +1,12 @@
 package wolox.training.repositories;
 
-import org.springframework.stereotype.Repository;
+
+import ch.qos.logback.core.pattern.parser.OptionTokenizer;
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
 import wolox.training.models.Book;
 
-public interface BookRepository extends Repository {
+public interface BookRepository extends CrudRepository<Book, Integer> {
 
-	public Book findBookByAuthor(String author);
+	public Optional<Book> findBookByAuthor(String author);
 }
